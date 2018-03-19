b4b26eadca7cc2e15fe2b645015f87cec1101da7
bigbluu722 <bigbluu722@gmail.com>
2018-03-19 07:36:39 -0400

    Added some stuff

--- src/com/bigbluu722/MoonStormInfo/templates/Messages.java
+++ src/com/bigbluu722/MoonStormInfo/templates/Messages.java
@@ -1,5 +1,17 @@
 package com.bigbluu722.MoonStormInfo.templates;
 
-public class Messages {
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.PluginDescriptionFile;
 
+public class Messages {
+	final static PluginDescriptionFile pdfFile = Bukkit.getPluginManager().getPlugin("AuroraInfo").getDescription();
+	static String version = pdfFile.getVersion();
+	
+	public static final String PREFIX = ChatColor.WHITE + "[" + ChatColor.LIGHT_PURPLE + "Au" + ChatColor.DARK_PURPLE
+			+ "ro" + ChatColor.DARK_BLUE + "ra" + ChatColor.BLUE + "C" + ChatColor.DARK_AQUA + "ra" + ChatColor.AQUA
+			+ "ft " + ChatColor.GOLD + "Info" + ChatColor.WHITE + "] ";
+
+	public static final String VERSION = ChatColor.GREEN + "Version " + ChatColor.GOLD + version + ChatColor.GREEN
+			+ " by bigbluu722" + ChatColor.WHITE + "\n";
 }
